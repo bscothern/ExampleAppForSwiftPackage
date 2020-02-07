7997c9c900f19fb933bff0389bdf6f3402ef9c3c
Braden Scothern <BradenScothern@gmail.com>
2020-02-06 17:33:53 -0700

    fixed build issue in the package tests preventing SwiftUI previews from working

--- Tests/ExamplePackageTests/ExamplePackageTests.swift
+++ Tests/ExamplePackageTests/ExamplePackageTests.swift
@@ -6,7 +6,7 @@ final class ExamplePackageTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(ExamplePackage().text, "Hello, from ExamplePackage!")
+        XCTAssertEqual(ExamplePackage.text, "Hello, from ExamplePackage!")
     }
 
     static var allTests = [
